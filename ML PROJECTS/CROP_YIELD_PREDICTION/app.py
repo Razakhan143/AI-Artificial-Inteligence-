2acc2fe2a8ba4e4a37ce6f45047a3229584b3012
Razakhan143 <razakhanzada100@gmail.com>
2024-07-11 13:05:24 +0500

    Initial commit

---
+++ ML PROJECTS/CROP_YIELD_PREDICTION/app.py
@@ -0,0 +1,35 @@
+from flask import Flask,request,render_template
+import numpy as np
+import sklearn
+import pickle
+
+#loading models
+dtr = pickle.load(open('AI projects\project_dtr.pkl','rb'))
+preprocessor = pickle.load(open('AI projects\project_preprocessor.pkl','rb'))
+#creating flask app
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('project_web.html')
+@app.route("/predict",methods=['POST'])
+def predict():
+    if request.method=='POST':
+        district= request.form['district']
+        label= request.form['label']
+        pesticides= request.form['pesticides']
+        temperature= request.form['temperature']
+        humidity= request.form['humidity']
+        rainfall= request.form['rainfall']
+
+    features = np.array([[district,	label,	pesticides,	temperature,	humidity,	rainfall]], dtype=object)
+
+    transformed_features = preprocessor.transform(features)
+
+    prediction = dtr.predict(transformed_features).reshape(1, -1)
+
+    return render_template('project_web.html',prediction=prediction)
+
+
+#python main
+if __name__=='__main__':
+    app.run(debug=True)
